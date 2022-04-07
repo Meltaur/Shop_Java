a707cfda339bb8d32c96f3118080a5e82b436251
Wojciech Fierek <fierekwojciech@gmail.com>
2022-04-07 12:44:35 +0200

    Przechodzenie między trzema zakładkami

--- src/main/java/com/example/projekt_sklep/sites/MainPageHandler.java
+++ src/main/java/com/example/projekt_sklep/sites/MainPageHandler.java
@@ -4,19 +4,29 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainPageHandler {
+
+    private static final String FXML_PATH = "src/main/resources/com/example/projekt_sklep/view/";
+
     @FXML
     private BorderPane mainBorderPane;
 
     @FXML
-    private void handleShowView(ActionEvent e) {
+    private void handleShowView(ActionEvent e) throws MalformedURLException {
         String view = (String) ((Node)e.getSource()).getUserData();
-        loadFXML(getClass().getResource(view));
+        File file = new File(FXML_PATH+view);
+        URL url = file.toURI().toURL();;
+        loadFXML(url);
     }
 
     private void loadFXML(URL url) {
